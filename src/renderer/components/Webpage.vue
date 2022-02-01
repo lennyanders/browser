3f9151cb256a671873be12e27c38aec3222ccb36
LennyAnders <hey@lenny.fyi>
2022-02-01 23:34:56 +0100

    display target url at the bottom left

--- src/renderer/components/Webpage.vue
+++ src/renderer/components/Webpage.vue
@@ -1,5 +1,9 @@
 <script lang="ts" setup>
+  import { ref } from 'vue';
   import { tabs } from '../stores/tabs';
+
+  const targetUrl = ref('');
+  const setTargetUrl = (event: Event & { url: string }) => (targetUrl.value = event.url);
 </script>
 
 <template>
@@ -9,17 +13,31 @@
       class="webpage__view"
       src="https://lenny.fyi"
       :hidden="!tab.active"
+      @update-target-url.passive="setTargetUrl"
     ></webview>
+    <span class="target-url" :hidden="!targetUrl">{{ targetUrl }}</span>
   </div>
 </template>
 
 <style lang="scss" scoped>
   .webpage {
     grid-area: webpage;
+    position: relative;
 
     &__view {
       width: 100%;
       height: 100%;
     }
+  }
+
+  .target-url {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    padding: 0 0.25rem;
+    background-color: var(--bg-color);
+    border: var(--border);
+    border-left: 0;
+    border-bottom: 0;
   }
 </style>

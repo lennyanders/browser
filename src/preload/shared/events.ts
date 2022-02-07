0fe704965a78d935547ae4b7d9e45a3aa630ab29
LennyAnders <hey@lenny.fyi>
2022-02-07 19:21:10 +0100

    create new tab with ctrl + t

--- src/preload/shared/events.ts
+++ src/preload/shared/events.ts
@@ -3,8 +3,10 @@ import { ipcRenderer } from 'electron';
 addEventListener(
   'keyup',
   (event) => {
+    if (event.ctrlKey && event.key === 't') return ipcRenderer.send('newTab');
+
     if (event.ctrlKey && event.key === 'Tab') {
-      ipcRenderer.send('setActiveTabByOffset', event.shiftKey ? -1 : 1);
+      return ipcRenderer.send('setActiveTabByOffset', event.shiftKey ? -1 : 1);
     }
   },
   { passive: true },
